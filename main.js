ba009a6cd226456150bb02c94c44552bfa24bb5f
meet273 <83627828+meet273@users.noreply.github.com>
2022-05-10 16:01:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let dayel = document.getElementById("day")
+let hoursel = document.getElementById("hours")
+let minsel = document.getElementById("mins")
+let secondel = document.getElementById("second")
+
+const newyear = "1 jan 2023"
+
+function countdown() {
+
+    let newyeardate = new Date(newyear);
+    let currentdate = new Date();
+
+    const totalsecond = (newyeardate - currentdate) / 1000;
+    const days = Math.floor(totalsecond / 3600 / 24);
+    const hours = Math.floor(totalsecond / 3600) % 24;
+    const minutes = Math.floor(totalsecond / 60) % 60;
+    const second = Math.floor(totalsecond) % 60
+
+    // console.log(days, hours, minutes, second);
+
+    dayel.innerHTML=timeformate(days);
+    hoursel.innerHTML=timeformate(hours);
+    minsel.innerHTML=timeformate(minutes);
+    secondel.innerHTML=timeformate(second);
+}
+function timeformate(time) {
+    return time<10 ? `0${time}`:time;
+}
+countdown();
+
+setInterval(countdown, 1000)
